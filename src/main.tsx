abe302322c854f3f4ddc8b43c9866bba758ab8d5
Aleksey Demchenkov <contact@pelfox.dev>
2025-11-22 17:00:42 +0300

    feat: prepare to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/learning-tracker/">
       <App />
     </BrowserRouter>
   </StrictMode>,
